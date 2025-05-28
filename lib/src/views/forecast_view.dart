bf47a91ec3ee2c218e53ac4178bc10bc05d9d2f3
issel <isselcode@gmail.com>
2025-05-28 14:26:51 -0600

    Mostrar Datos en la interfaz pt 2 - Pronostico

--- lib/src/views/forecast_view.dart
+++ lib/src/views/forecast_view.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_material_symbols/flutter_material_symbols.dart';
 import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/core/utils/get_weather_icon_util.dart';
+import 'package:weather_app/src/clean_features/entities/forecast_day_entity.dart';
 import 'package:weather_app/src/clean_features/widgets/current_weather_widget.dart';
+import 'package:weather_app/src/controllers/weather_controller.dart';
 
 import '../../core/app/consts.dart';
 import '../clean_features/widgets/current_weather_extra_info_widget.dart';
 import '../clean_features/widgets/weather_tile_widget.dart';
 
-class ForecastView extends StatelessWidget {
+class ForecastView extends StatefulWidget {
   const ForecastView({super.key});
+
+  @override
+  State<ForecastView> createState() => _ForecastViewState();
+}
+
+class _ForecastViewState extends State<ForecastView> {
+
+  late ForecastDayEntity selectedForecastDay;
+  late WeatherController weatherController;
+
+  @override
+  void initState() {
+    super.initState();
+    weatherController = context.read();
+    selectedForecastDay = weatherController.weather!.forecast.forecastday.first;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,10 +51,13 @@ class ForecastView extends StatelessWidget {
           children: [
 
             CurrentWeatherWidget(
-              title: "Lluvia y truenos",
-              temperature: "16/18",
-              date: DateTime.now(),
-              icon: "assets/weather_icons/PartlyCloudyDark.png"
+              title: selectedForecastDay.day.condition.text,
+              temperature: selectedForecastDay.day.maxtempC.toStringAsFixed(0),
+              date: selectedForecastDay.date,
+              icon: getWeatherIconWithEnumUtil(
+                iconEnum: selectedForecastDay.day.condition.code,
+                isDark: false
+              ).fold((l) => throw Exception(l.message), (r) => r,)
             ),
 
             const Gap(25),
@@ -44,7 +68,7 @@ class ForecastView extends StatelessWidget {
               children: [
                 CurrentWeatherExtraInfoWidget(
                     icon: Icons.air_outlined,
-                    title: "8km/h",
+                    title: "${selectedForecastDay.day.maxwindKph}km/h",
                     description: "Viento"
                 ),
                 SizedBox(
@@ -52,17 +76,17 @@ class ForecastView extends StatelessWidget {
                     child: VerticalDivider(color: AppColors.neutralGrey,)
                 ),
                 CurrentWeatherExtraInfoWidget(
-                    icon: MaterialSymbols.rainy,
-                    title: "72%",
-                    description: "Lluvia"
+                  icon: MaterialSymbols.rainy,
+                  title: "${selectedForecastDay.day.dailyChanceOfRain}%",
+                  description: "Lluvia"
                 ),
                 SizedBox(
-                    height: 80,
-                    child: VerticalDivider(color: AppColors.neutralGrey,)
+                  height: 80,
+                  child: VerticalDivider(color: AppColors.neutralGrey,)
                 ),
                 CurrentWeatherExtraInfoWidget(
                     icon: MaterialSymbols.humidity_percentage_filled,
-                    title: "22%",
+                    title: "${selectedForecastDay.day.avghumidity}%",
                     description: "Humedad"
                 )
               ],
@@ -73,16 +97,26 @@ class ForecastView extends StatelessWidget {
             // Lista de clima por Hora
             Expanded(
               child: ListView.separated(
-                itemCount: 7,
+                itemCount: weatherController.weather!.forecast.forecastday.length,
                 separatorBuilder: (context, index) => Gap(10),
                 itemBuilder: (context, index) {
+
+                  ForecastDayEntity forecastDay = weatherController.weather!.forecast.forecastday[index];
+
                   return WeatherTileWidget(
                     onTap: () {
 
+                      selectedForecastDay = forecastDay;
+                      setState(() {});
+
                     },
-                    leading: "12:00",
-                    trailing: "18",
-                    icon: "assets/weather_icons/SnowyDark.png"
+                    selected: forecastDay == selectedForecastDay,
+                    leading: DateFormat("EEEE, dd").format(forecastDay.date),
+                    trailing: "${forecastDay.day.maxtempC.toStringAsFixed(0)}°. ${forecastDay.day.mintempC.toStringAsFixed(0)}",
+                    icon: getWeatherIconWithEnumUtil(
+                      iconEnum: forecastDay.day.condition.code,
+                      isDark: false
+                    ).fold((l) => throw Exception(l.message), (r) => r,)
                   );
                 },
               ),
